41f2f327452034da73fc0e673ba741cb3c160461
Md. Chanchal Biswas <mchanchalbd@gmail.com>
2023-09-13 12:30:51 +0600

    PHP Basic Syntax

---
+++ language-reference/syntax.php
@@ -0,0 +1,29 @@
+<?php
+// PHP basic syntax
+// PHP Tags (Opening Tag and Closing Tag)
+// We cant write PHP code any place in a PHP file
+// Print
+// PHP Parser
+// Escaping HTML, CSS, JS
+// PHP Comments (Single line comment and Multiple line comment)
+
+echo "PHP Programming"; // Statement 1 and print `PHP Programming`
+echo "\n"; // Statement 2 and print line break `\n`
+echo "Java Programming"; // Statement 3 and print `Java Programming`
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Web Developmnet Course</h1>
+    <?php
+    // Write PHP Code
+    ?>
+</body>
+</html>
+<?php
+// Write PHP Code and don't need closing tag
